3dd9475d3b3b251e18b23ae4dd78f2c6b7868170
Velikiy Vladislav <velikiy.workmail@gmail.com>
2020-11-13 01:09:14 +0200

    Updates

---
+++ precache-manifest.39fb6b730b448ea9041bc0f207d11820.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304859f0126a1f5e7a6099aacf0f2855",
+    "url": "/iss-tracker/index.html"
+  },
+  {
+    "revision": "064e006f9daaf8f0fa74",
+    "url": "/iss-tracker/static/css/main.a02eef18.chunk.css"
+  },
+  {
+    "revision": "5f65a227f07b37625eaf",
+    "url": "/iss-tracker/static/js/2.cfa09632.chunk.js"
+  },
+  {
+    "revision": "2f4640ee3b5cd44c62238023a1389710",
+    "url": "/iss-tracker/static/js/2.cfa09632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064e006f9daaf8f0fa74",
+    "url": "/iss-tracker/static/js/main.d87037ed.chunk.js"
+  },
+  {
+    "revision": "be51e70403e185e18abc",
+    "url": "/iss-tracker/static/js/runtime-main.3b626b92.js"
+  },
+  {
+    "revision": "442ed8f30587981177c88968f3cda84e",
+    "url": "/iss-tracker/static/media/icon.442ed8f3.svg"
+  },
+  {
+    "revision": "7de11697ef2fcc652a063b4e44fa082e",
+    "url": "/iss-tracker/static/media/portrait-placeholder.7de11697.png"
+  }
+]);
